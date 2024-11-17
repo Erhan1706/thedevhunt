cd7f18d6f0025c31893783dd62b2b947a805550a
Dorian <dorian.erhan@gmail.com>
2024-11-17 22:00:53 +0100

    Create gunicorn config file

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,5 +1,20 @@
+import os
+
+# Create logs directory if it doesn't exist
+log_path = "/home/django/thedevhunt/logs"
+os.makedirs(log_path, exist_ok=True)
+
 bind = "unix:/home/django/thedevhunt/thedevhunt.sock"
-workers = 3
 acesslog = "/home/django/thedevhunt/logs/gunicorn-access.log"
 errorlog = "/home/django/thedevhunt/logs/gunicorn-error.log"
 loglevel = "info"
+
+workers = 3
+worker_class = 'gstreamer'
+
+wsgi_app = 'thedevhunt.wsgi:application'
+proc_name = 'thedevhunt'
+
+# Additional settings
+timeout = 30
+keepalive = 2
