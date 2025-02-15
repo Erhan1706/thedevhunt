3828972a8a7ba96b7a79c6413084a3a8011d353a
Dorian <dorian.erhan@gmail.com>
2025-02-15 19:59:39 +0100

    Fix celery worker command

--- scripts/prod_web_start.sh
+++ scripts/prod_web_start.sh
@@ -5,5 +5,5 @@ python manage.py collectstatic
 echo -e "\nCollected static files"
 
 python manage.py migrate
-gunicorn thedevhunt.wsgi:application 
+gunicorn --bind 0.0.0.0:8000 thedevhunt.wsgi:application 
 #python manage.py runserver 0.0.0.0:8000
