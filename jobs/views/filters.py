ba843db928e44cf3eb858178cff030cc4b4c53c7
Dorian <dorian.erhan@gmail.com>
2025-02-24 15:11:03 +0100

    Major refactorings in views and add search functionality by text

--- jobs/views/filters.py
+++ jobs/views/filters.py
@@ -1,100 +1,111 @@
 from django.shortcuts import render
 from ..models import Job
 from django.views.decorators.http import require_http_methods
-from django.shortcuts import get_object_or_404
 from django.db.models import Q
-from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 from django.template import loader
 from .util import categories, eu_countries, get_page_obj
+from django.http import HttpRequest
+import copy
+
+DEFAULT_FILTERS = {'location': [], 'role': [], 'company': [], 'search': "", 'remote': False}
+FILTER_KEYS = ["remote", "location", "company", "role", "search"]
+
+def get_session_filters(request: HttpRequest) -> dict:
+    return request.session.get('filters', copy.deepcopy(DEFAULT_FILTERS))
 
 @require_http_methods(["GET"])
-def get_filters(request):
-    return render(request, 'jobs/partials/current_filters_partial.html', {"current_filters": request.session.get('filters')})
-
+def get_filters(request: HttpRequest) -> HttpResponse:
+    return render(request, 'jobs/partials/current_filters_partial.html', {"current_filters": get_session_filters(request)})
 
 @require_http_methods(["GET"])
-def update_company_list(request):
-    filters = request.session.get('filters', {'locations': [], 'roles': [], 'technologies': [], 'companies': []})
+def update_company_list(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     companies = Job.objects.values('company').distinct().order_by('company')
-    return render(request, 'jobs/partials/company_filter_partial.html', {"filtered_companies": filters['companies'],
+    return render(request, 'jobs/partials/company_filter_partial.html', {"filtered_companies": filters['company'],
                   "companies": companies})
 
 @require_http_methods(["GET"])
-def update_role_list(request):
-    filters = request.session.get('filters', {'locations': [], 'roles': [], 'technologies': [], 'companies': []})
+def update_role_list(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     return render(request, 'jobs/partials/category_filter_partial.html', {"categories": categories,
                   "current_filters": filters})
 
-
-@require_http_methods(["POST"])
-def add_filter(request):
-    filters = request.session.get('filters', {'locations': [], 'roles': [], 'technologies': [], 'companies': []})
-    remote = request.POST.get('remote')
-    location = request.POST.get('country')
-    company = request.POST.get('company')
-    role = request.POST.get('role')
-
-    if remote:
-        if remote == 'on':
-            filters['remote'] = True
-        if remote == 'off':
-            return remove_filter(request)
-    elif location and not location in filters['locations']:
-        filters['locations'].append(location)
-    elif company and not company in filters['companies'] :
-        filters['companies'].append(company)
-    elif role and not role in filters['roles']:
-        filters['roles'].append(role)
-
+def render_job_list(request: HttpRequest, filters: dict) -> HttpResponse:
+    """
+    Utility function to render the job list based on the filters provided. 
+    """
     query_set = process_filters(filters)
     jobs = Job.objects.filter(query_set).order_by('-created_at')
     page_obj = get_page_obj(jobs)
-
+    
     request.session['filters'] = filters
     template = loader.get_template('jobs/job_list.html')
     response = HttpResponse(template.render({"page_obj": page_obj, "current_filters": filters}, request))
     response.headers["HX-Trigger"] = "filterChanged"
-    return response 
+    return response
+
+@require_http_methods(["POST"])
+def add_filter(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
+    for key in FILTER_KEYS:
+        value = request.POST.get(key)
+        if value and key in filters and (key == 'remote' or value not in filters[key]) :
+            if key == 'search':
+                filters[key] = value
+            elif key == 'remote' and value == 'on':
+                filters[key] = True
+            elif key == 'remote' and value == 'off':
+                return remove_filter(request)
+            else:
+                filters[key].append(value)
+        elif key == 'search' and not value:
+            filters[key] = ""
+
+    return render_job_list(request, filters)
 
 @require_http_methods(["GET"])
-def update_country_list(request):
-    filters = request.session.get('filters', {'locations': [], 'roles': [], 'technologies': [], 'companies': []})
+def update_country_list(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     show_all = request.session.get('show_all_countries', False)
     if show_all:
         rendered_countries = eu_countries
     else:
         rendered_countries = eu_countries[:12]
 
-    return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['locations'],
+    return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['location'],
                   "eu_countries": rendered_countries, "show_all_countries": show_all})
 
 
-def process_filters(filters):
+def process_filters(filters) -> Q:
+    """
+    Processes the filters dict and return a Q object that can be used to filter the jobs from the db.
+    """
     queries = {
-        'locations': Q(),
-        'roles': Q(),
-        'technologies': Q(),
-        'companies': Q(),
+        'location': Q(),
+        'role': Q(),
+        'company': Q(),
     }
     query_set = Q()
     for key, values in filters.items():
-        if key=='locations'and values:
-            queries[key] = add_query_filters(queries[key], 'location', values)
-        elif key=='roles' and values:
+        if key in ['location', "company"] and values:
+            queries[key] = add_query_filters(queries[key], key, values)
+        elif key=='role' and values:
             queries[key] = process_role_filters(values)
-        elif key=='technologies' and values:
-            queries[key] = add_query_filters(queries[key], 'technologies', values)
-        elif key=='companies' and values:
-            queries[key] = add_query_filters(queries[key], 'company', values)
         elif key == 'remote' and values:
             queries['remote'] = Q(remote=True)
+        elif key == 'search' and values:
+            queries['title'] = Q(title__icontains=values)
 
     for query in queries.values():
         query_set &= query
     return query_set
 
-def add_query_filters(query_set, key, filters):
+def add_query_filters(query_set, key, filters) -> Q:
+    """
+    Logical OR operation on filters for the same key. For example, if the filters are ['Germany', 'France'],
+    the results should include jobs from Germany or France.
+    """
     for filter in filters:
         query_set |= Q(**{f"{key}__icontains": filter})
     return query_set
@@ -117,62 +128,34 @@ def process_role_filters(roles):
     return query_set
 
 @require_http_methods(["POST"])
-def remove_filter(request):
-    filters = request.session.get('filters')
+def remove_filter(request: HttpRequest) -> HttpResponse:
+    filters =  get_session_filters(request)
     if not filters:
         return HttpResponse(status=400)
     
-    location = request.POST.get('country')
-    remote = request.POST.get('remote')
-    company = request.POST.get('company')
-    role = request.POST.get('role')
-
-    if remote and remote == 'off':
-        filters.pop('remote')
-    elif location:
-        filters['locations'].remove(location)
-    elif company:
-        filters['companies'].remove(company)
-    elif role:
-        filters['roles'].remove(role)
-
-    query_set = process_filters(filters)
-    jobs = Job.objects.filter(query_set).order_by('-created_at')
-    request.session['filters'] = filters
-    page_obj = get_page_obj(jobs)
-
-    template = loader.get_template('jobs/job_list.html')
-    response = HttpResponse(template.render({"page_obj": page_obj, "current_filters": filters}, request))
-    response.headers["HX-Trigger"] = "filterChanged"
-    return response 
-
+    for key in FILTER_KEYS:
+        value = request.POST.get(key)
+        if value and key in filters and value in filters[key]:
+            if key =="remote":
+                filters.pop('remote')
+            else:
+                filters[key].remove(value)
+    return render_job_list(request, filters)
 
 @require_http_methods(["GET"])
-def get_filters(request):
-    return render(request, 'jobs/partials/current_filters_partial.html', {"current_filters": request.session.get('filters')})
-
-@require_http_methods(["GET"])
-def render_filters_mobile(request):
-    filters = request.session.get('filters',{'locations': [], 'roles': [], 'technologies': [], 'companies':[]})
+def render_filters_mobile(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     companies = Job.objects.values('company').distinct().order_by('company')
     show = request.GET.get('show')
     request.session['show_all_countries'] = False
 
     if show == "True":
-        return render(request, 'jobs/filters.html', {"current_filters": filters, "filtered_locations": filters['locations'],
+        return render(request, 'jobs/filters.html', {"current_filters": filters, "filtered_locations": filters['location'],
                                 "eu_countries": eu_countries[:12], "companies": companies, "mobile_display": True, 
-                                "categories": categories, "companies": companies, "filtered_companies": filters['companies'],
-                                "show_all_countries": False})
+                                "categories": categories, "filtered_companies": filters['company'], "show_all_countries": False})
     else:
         return render(request, 'jobs/partials/mobile_filter_button_partial.html', {})
 
 @require_http_methods(["POST"])
-def clear_filters(request):
-    request.session['filters'] = {'locations': [], 'roles': [], 'technologies': [], 'companies': []}
-    jobs = Job.objects.all().order_by('-created_at')
-    page_obj = get_page_obj(jobs)
-
-    template = loader.get_template('jobs/job_list.html')
-    response = HttpResponse(template.render({"page_obj": page_obj, "current_filters": request.session.get('filters')}, request))
-    response.headers["HX-Trigger"] = "filterChanged"
-    return response
+def clear_filters(request: HttpRequest) -> HttpResponse:
+    return render_job_list(request, DEFAULT_FILTERS)
--- jobs/views/main.py
+++ jobs/views/main.py
@@ -3,16 +3,17 @@ from ..models import Job
 import ast
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
-from .filters import process_filters
+from .filters import process_filters, get_session_filters
 from .util import eu_countries, categories, get_page_obj
+from django.http import HttpRequest, HttpResponse
 
 """
 Loads the main page with all the job listings. Content.html is the base layout + filter layout + some extra information.
 Within content.html, job_list.html is loaded with the job listings.
 """
 @require_http_methods(["GET"])
-def load_main_page(request):
-    filters = request.session.get('filters',{'locations': [], 'roles': [], 'technologies': [], 'companies':[]})
+def load_main_page(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     request.session['show_all_countries'] = False
     query_set = process_filters(filters)
     jobs = Job.objects.filter(query_set).order_by('-created_at')
@@ -21,12 +22,15 @@ def load_main_page(request):
     page_obj = get_page_obj(jobs)
 
     return render(request, 'jobs/content.html', {"eu_countries": eu_countries[:12], "page_obj": page_obj, "companies": companies,
-                "show_all_countries": False, "current_filters": filters, "filtered_locations": filters['locations'],
-                "filtered_companies": filters['companies'], "categories": categories})
+                "show_all_countries": False, "current_filters": filters, "filtered_locations": filters['location'],
+                "filtered_companies": filters['company'],"categories": categories})
 
 @require_http_methods(["GET"])
-def fetch_page(request):
-    filters = request.session.get('filters',{'locations': [], 'roles': [], 'technologies': [], 'companies': []})
+def fetch_page(request: HttpRequest) -> HttpResponse:
+    """
+    Fetches the individual listing page for a singular job. 
+    """
+    filters = get_session_filters(request)
     query_set = process_filters(filters)
     jobs = Job.objects.filter(query_set).order_by('-created_at')
 
@@ -36,7 +40,7 @@ def fetch_page(request):
 
 
 @require_http_methods(["GET"])
-def get_individual_listing(request, company, opening):
+def get_individual_listing(request: HttpRequest, company: str, opening: str) -> HttpResponse:
     if company == "booking_com":
         company = "booking.com"
         
@@ -44,19 +48,19 @@ def get_individual_listing(request, company, opening):
     return render(request, 'jobs/listing.html', {"job": job})
 
 @require_http_methods(["GET"])
-def show_all_listing_locations(request, locations):
+def show_all_listing_locations(request: HttpRequest, locations) -> HttpResponse:
     locations  = ast.literal_eval(locations)
     context = {"locations": locations[3:]}
     return render(request, 'jobs/partials/listing_locations_partial.html', context)
 
 @require_http_methods(["GET"])
-def show_all_countries(request):
-    filters = request.session.get('filters', {'locations': [], 'roles': [], 'technologies': [], 'companies': []})
+def show_all_countries(request: HttpRequest) -> HttpResponse:
+    filters = get_session_filters(request)
     if request.GET.get('show_all') == 'True':
         request.session['show_all_countries'] = True
-        return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['locations'],
+        return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['location'],
                       "eu_countries": eu_countries[12:], "show_all_countries": True})
     else:
         request.session['show_all_countries'] = False
-        return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['locations'],
+        return render(request, 'jobs/partials/country_filter_partial.html', {"filtered_locations": filters['location'],
                       "eu_countries": eu_countries[:12], "show_all_countries": False})
